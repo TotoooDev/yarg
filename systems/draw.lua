7a381f5de2467b90b4665c8c6a72d48f0103e920
= <=>
2025-03-19 14:37:41 +0100

    add overheat gauge

--- systems/draw.lua
+++ systems/draw.lua
@@ -1,29 +1,59 @@
 local Concord = require("lib.Concord")
 
 local DrawSystem = Concord.system({
-    pool = { "transform", "drawable" }
+    pool = { "transform", "drawable" },
+    playerPool = { "transform", "drawable", "player" }
 })
+
+function DrawSystem:default(entity)
+    if entity.drawable.image then
+        love.graphics.draw(
+            entity.drawable.image,
+            entity.transform.pos.x - entity.transform.size.x / 2,
+            entity.transform.pos.y - entity.transform.size.y / 2,
+            0.0,
+            entity.transform.size.x,
+            entity.transform.size.y
+        )
+    else
+        love.graphics.rectangle(
+            "fill",
+            entity.transform.pos.x - entity.transform.size.x / 2,
+            entity.transform.pos.y - entity.transform.size.y / 2,
+            entity.transform.size.x,
+            entity.transform.size.y
+        )
+    end
+end
+
+function DrawSystem:player(entity)
+    local player = entity.player
+
+    -- draw heat gauge
+    if player.overheating then
+        love.graphics.setColor({ 1.0, 0.0, 0.0 })
+    else
+        love.graphics.setColor({ 1.0, 1.0, 1.0 })
+    end
+
+    love.graphics.rectangle(
+        "fill",
+        0,
+        love.graphics.getHeight() / (player.overheatLimit / player.heat),
+        10.0,
+        love.graphics.getHeight()
+    )
+    
+    love.graphics.setColor({ 1.0, 1.0, 1.0 })
+end
 
 function DrawSystem:draw()
     for _, entity in ipairs(self.pool) do
-        if entity.drawable.image then
-            love.graphics.draw(
-                entity.drawable.image,
-                entity.transform.pos.x - entity.transform.size.x / 2,
-                entity.transform.pos.y - entity.transform.size.y / 2,
-                0.0,
-                entity.transform.size.x,
-                entity.transform.size.y
-            )
-        else
-            love.graphics.rectangle(
-                "fill",
-                entity.transform.pos.x - entity.transform.size.x / 2,
-                entity.transform.pos.y - entity.transform.size.y / 2,
-                entity.transform.size.x,
-                entity.transform.size.y
-            )
-        end
+        self:default(entity)
+    end
+
+    for _, entity in ipairs(self.playerPool) do
+        self:player(entity)
     end
 end
 
--- systems/player.lua
+++ systems/player.lua
@@ -6,46 +6,50 @@ local PlayerSystem = Concord.system({
     pool = { "transform", "ship", "player" }
 })
 
+local function move(entity, dt)
+    local transform = entity.transform
+    local windowWidth = love.graphics.getWidth()
+
+    if Input.left and transform.pos.x - transform.size.x / 2 >= 0.0 then
+        transform.pos.x = transform.pos.x - entity.ship.speed * dt
+    end
+
+    if Input.right and transform.pos.x + transform.size.x / 2 <= windowWidth then
+        transform.pos.x = transform.pos.x + entity.ship.speed * dt
+    end
+end
+
 function PlayerSystem:update(dt)
     for _, entity in ipairs(self.pool) do
         local transform = entity.transform
         local player = entity.player
 
-        if Input.left then
-            transform.pos.x = transform.pos.x - entity.ship.speed * dt
+        move(entity, dt)
+
+        local canShoot = love.timer.getTime() - player.lastShoot >= player.cooldown and not player.overheating
+        if Input.shoot and canShoot then
+            entity.world:newEntity("projectile")
+                :give("transform", transform.pos:clone(), nil, Vector(10.0, 32.0))
+                :give("projectile")
+                :give("drawable")
+
+            -- overheat
+            player.heat = player.heat + player.heatRate
+            if player.heat >= player.overheatLimit then
+                player.overheating = true
+            end
+
+            player.lastShoot = love.timer.getTime()
         end
 
-        if Input.right then
-            transform.pos.x = transform.pos.x + entity.ship.speed * dt
+        -- dissipate heat
+        if player.heat >= 0.0 then
+            player.heat = player.heat - player.overheatDissipationRate * dt
         end
-
-        if Input.shoot and love.timer.getTime() - player.lastShoot >= player.cooldown then
-            if not player.overheating then
-                entity.world:newEntity("projectile")
-                    :give("transform", transform.pos:clone())
-                    :give("projectile")
-                    :give("drawable")
-
-                -- overheat
-                player.heat = player.heat + player.heatRate
-                if player.heat >= player.overheatLimit then
-                    player.overheating = true
-                end
-
-                player.lastShoot = love.timer.getTime()
-            end
-        else
-            -- dissipate heat
-            if player.heat >= 0.0 then
-                player.heat = player.heat - player.overheatDissipationRate * dt
-            end
-            if player.heat < 0.0 then
-                player.overheating = false
-                player.heat = 0.0
-            end
+        if player.heat < 0.0 then
+            player.overheating = false
+            player.heat = 0.0
         end
-
-        print(player.heat)
     end
 end
 
