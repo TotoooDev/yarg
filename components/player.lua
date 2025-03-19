e8b96f86c2fc2fa6e57edb711df96521bb24b15d
= <=>
2025-03-19 14:17:46 +0100

    add overheating mechanic

--- components/player.lua
+++ components/player.lua
@@ -3,4 +3,10 @@ local Concord = require("lib.Concord")
 Concord.component("player", function (c)
     c.cooldown = 0.1 -- cooldown in seconds
     c.lastShoot = 0.0
+
+    c.heat = 0.0
+    c.heatRate = 10.0
+    c.overheatLimit = 100.0
+    c.overheatDissipationRate = 50.0
+    c.overheating = false
 end)
--- systems/player.lua
+++ systems/player.lua
@@ -8,22 +8,44 @@ local PlayerSystem = Concord.system({
 
 function PlayerSystem:update(dt)
     for _, entity in ipairs(self.pool) do
+        local transform = entity.transform
+        local player = entity.player
+
         if Input.left then
-            entity.transform.pos.x = entity.transform.pos.x - entity.ship.speed * dt
+            transform.pos.x = transform.pos.x - entity.ship.speed * dt
         end
 
         if Input.right then
-            entity.transform.pos.x = entity.transform.pos.x + entity.ship.speed * dt
+            transform.pos.x = transform.pos.x + entity.ship.speed * dt
         end
 
-        if Input.shoot and love.timer.getTime() - entity.player.lastShoot >= entity.player.cooldown then
-            entity.world:newEntity("projectile")
-                :give("transform", entity.transform.pos:clone())
-                :give("projectile")
-                :give("drawable")
+        if Input.shoot and love.timer.getTime() - player.lastShoot >= player.cooldown then
+            if not player.overheating then
+                entity.world:newEntity("projectile")
+                    :give("transform", transform.pos:clone())
+                    :give("projectile")
+                    :give("drawable")
 
-            entity.player.lastShoot = love.timer.getTime()
+                -- overheat
+                player.heat = player.heat + player.heatRate
+                if player.heat >= player.overheatLimit then
+                    player.overheating = true
+                end
+
+                player.lastShoot = love.timer.getTime()
+            end
+        else
+            -- dissipate heat
+            if player.heat >= 0.0 then
+                player.heat = player.heat - player.overheatDissipationRate * dt
+            end
+            if player.heat < 0.0 then
+                player.overheating = false
+                player.heat = 0.0
+            end
         end
+
+        print(player.heat)
     end
 end
 
